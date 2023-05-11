b0795f42d7254a35f41260b36329dfa5b5b4f509
TroyaDevelop <goddys777@icloud.com>
2023-05-11 20:39:57 +0300

    first update

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function changeHair(imageUrl) {
+    const hair = document.querySelector('.hair');
+    hair.style.backgroundImage = `url(${imageUrl})`;
+  }
+  
+  function changeEyes(imageUrl) {
+    const eyes = document.querySelector('.eyes');
+    eyes.style.backgroundImage = `url(${imageUrl})`;
+  }
+  
+  function changeMouth(imageUrl) {
+    const mouth = document.querySelector('.mouth');
+    mouth.style.backgroundImage = `url(${imageUrl})`;
+  }
