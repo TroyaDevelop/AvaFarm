5389d78ce1a6cd4350de98b78bccb309390dc480
TroyaDevelop <goddys777@icloud.com>
2023-05-12 20:51:31 +0300

    resolution test 2

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 'use strict';
 
-function init(){
-  const widthOutput = 480;
-
-  if(outerWidth <= widthOutput){
-    document.body.style.transform = 'rotate(90deg)';
-    screen.orientation.lock("landscape");
-  }
-};
-
 function changeHair(imageUrl) {
     const hair = document.querySelector('.hair');
     hair.style.backgroundImage = `url(${imageUrl})`;
@@ -28,5 +19,3 @@ function changeHair(imageUrl) {
     const mouth = document.querySelector('.mouth');
     mouth.style.backgroundImage = `url(${imageUrl})`;
   }
-
-  init();
